8efd37e6a6e1c5fc75e8ee48e042690edd2b4f4c
sarahcruzz <sarah.cruz0492@gmail.com>
2024-08-14 22:49:15 -0300

    fazendo exercícios da aula 3

---
+++ Aula03/ex1ao15/ex1.js
@@ -0,0 +1,4 @@
+const frase = "desenvolvimento backend js"
+
+console.log(frase.length)
+console.log(frase.toUpperCase())
---
+++ Aula03/ex1ao15/ex2.js
@@ -0,0 +1,5 @@
+const valor1 = null
+let valor2
+
+console.log(valor1)
+console.log(valor2)
